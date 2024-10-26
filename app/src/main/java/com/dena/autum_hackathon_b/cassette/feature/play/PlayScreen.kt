0b38a2c73968acbefd92e026e42f71578c63996b
mio_osato <sugar_sugar2000@icloud.com>
2024-10-26 18:16:26 +0900

    基本的なUIの完成

--- app/src/main/java/com/dena/autum_hackathon_b/cassette/feature/play/PlayScreen.kt
+++ app/src/main/java/com/dena/autum_hackathon_b/cassette/feature/play/PlayScreen.kt
@@ -1,13 +1,21 @@
 package com.dena.autum_hackathon_b.cassette.feature.play
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -18,9 +26,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dena.autum_hackathon_b.cassette.R
 import com.dena.autum_hackathon_b.cassette.ui.theme.CassetteTheme
 
 @Composable
@@ -52,22 +68,88 @@ fun PlayScreen(modifier: Modifier = Modifier, screenState: PlayScreenState) {
             NavigationBar {
                 NavigationBarItem(
                     selected = false,
-                    onClick = {},
-                    icon = {Icon(Icons.Default.PlayArrow, "play")},
+                    onClick = {/*TODO:ここでPlay_screenへのナビゲーション行う*/},
+                    icon = { Icon(
+                        painter = painterResource(id = R.drawable.music_note),
+                        contentDescription = "play"
+                        ) },
                     modifier = Modifier,
                     label = {Text("Play")},
                 )
                 NavigationBarItem(
                     selected = false,
-                    onClick = {},
+                    onClick = {/*TODO:ここでCreate_screenへのナビゲーション行う*/},
                     icon = {Icon(Icons.Default.Edit, "create")},
                     modifier = Modifier,
                     label = {Text("Create")},
                     )
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {/* TODO: 後でセトリの表示 */},
+                modifier = Modifier,
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.description),
+                    contentDescription = "description"
+                )
+            }
         }
     ) { innerPadding ->
-        Text(text = screenState.cassetteId, modifier = Modifier.padding(innerPadding))
+        Column(
+            modifier = Modifier.padding(innerPadding)
+                .padding(top = 40.dp)
+        ) {
+            Text(
+                text = "cassette_name",
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 24.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "created_at",
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center,
+            )
+            Image(
+                painter = painterResource(id = R.drawable.cassette_image),
+                contentDescription = "Figure Image",
+                modifier = Modifier
+                    .fillMaxWidth() // 必要に応じてサイズを指定
+                    .padding(top = 16.dp, bottom = 30.dp)
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround,
+            ){
+                Icon(
+                    painter = painterResource(id = R.drawable.fast_rewind),
+                    contentDescription = "rewind",
+                    modifier = Modifier.size(60.dp),
+                )
+                Icon(
+                    Icons.Default.PlayArrow,
+                    "play",
+                    modifier = Modifier.size(60.dp),
+                    )
+                Icon(
+                    painter = painterResource(id = R.drawable.fast_forward),
+                    contentDescription = "forward",
+                    modifier = Modifier.size(60.dp),
+                )
+            }
+            Text(
+                text = "time",
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 20.dp),
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
 
