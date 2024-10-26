bcf1cb34e1280ae3ea9f3af0e8e2028022cbafa5
mio_osato <sugar_sugar2000@icloud.com>
2024-10-26 16:13:51 +0900

    トップバーとナビゲーションバーの追加

--- app/src/main/java/com/dena/autum_hackathon_b/cassette/feature/play/PlayScreen.kt
+++ app/src/main/java/com/dena/autum_hackathon_b/cassette/feature/play/PlayScreen.kt
@@ -2,8 +2,18 @@ package com.dena.autum_hackathon_b.cassette.feature.play
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -27,9 +37,36 @@ fun PlayScreenHost(
     PlayScreen(screenState = screenState)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayScreen(modifier: Modifier = Modifier, screenState: PlayScreenState) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+            title = {Text("App_name")},
+            modifier = Modifier,
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = false,
+                    onClick = {},
+                    icon = {Icon(Icons.Default.PlayArrow, "play")},
+                    modifier = Modifier,
+                    label = {Text("Play")},
+                )
+                NavigationBarItem(
+                    selected = false,
+                    onClick = {},
+                    icon = {Icon(Icons.Default.Edit, "create")},
+                    modifier = Modifier,
+                    label = {Text("Create")},
+                    )
+            }
+        }
+    ) { innerPadding ->
         Text(text = screenState.cassetteId, modifier = Modifier.padding(innerPadding))
     }
 }
